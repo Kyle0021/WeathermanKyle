c7859dad151b940190feaf7c6a1b2a270e2ed40b
Kyle <kfernandez@e4.co.za>
2019-07-03 22:03:29 +0200

    Initial Commit

---
+++ Weatherman Kyle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weatherman Kyle
+//
+//  Created by Kyle Carlos Fernandez on 2019/07/03.
+//  Copyright © 2019 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
